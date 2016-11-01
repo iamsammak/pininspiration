fbd174a5e3ef4ac85a95c88d863a2a95f96a712a
Samuel Mak <samuelswmak@gmail.com>
2016-11-01 10:41:50 -0700

    initial user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resource :session, only: [:create, :destroy, :show]
+  end
+  
   root to: 'static_pages#root'
 end
